6a4b4857e555598debcf362412712def58ba9ebc
Jennifer Nguyen <jennnguyen@csumb.edu>
2017-09-22 06:11:03 +0000

    adding comments to code

--- index.php
+++ index.php
@@ -12,20 +12,21 @@
         <?php
         
             //play();
-            // Player 1-4 are dealt cards
+            // Player 1-4 are dealt cards and scores are printed
         for ($i = 0; $i < 4; $i++) {
             $score[$i] = playerturn($deck);
             echo 'score '.$score[$i].'<br/>';
             //array_push($score, playerturn($deck));
         }
             
-        //$winner
-        $highestScore = 0;
-        $totalScore = 0;
-        $winnerIndex = -1;
-    
+        
+        $highestScore = 0; // find the highest score closed to 42
+        $totalScore = 0; // Calculate total score for the winner
+        $winnerIndex = -1; // To hold the index of the 'winner'
+        
+        // For loop used to find the highest score closed to 42
         for ($i = 0; $i < 4; $i++) {
-            if ($score[$i] > 42)
+            if ($score[$i] > 42) // players with scores above 42 automatically lose
                 continue;
             if ($score[$i] > $highestScore) {
                 $highestScore = $score[$i];
@@ -33,19 +34,20 @@
             }
         }
             
-        //IF THERE ARE NO WINNERS !!
+        //IF THERE ARE NO WINNERS !! (all scores are about 42)
         if ($winnerIndex == -1){
             echo "<h2>No winners.</h2>"; 
             return;
         }
-            
+        
+        // Adding all the points to the $totalScore, not including the winner's score
         for ($i = 0; $i < 4; $i++) {
             if ($i == $winnerIndex)
                 continue;
             $totalScore = $totalScore + $score[$i];
         }
         
-        $playerWinner = $winnerIndex + 1;
+        $playerWinner = $winnerIndex + 1; // index + 1 = player#
         switch($playerWinner)
             {
                 case 1: $winner = "Alex";
