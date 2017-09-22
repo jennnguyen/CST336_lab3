86600c59736b5d88a212027ae72b1ecd26d7e33c
Jennifer Nguyen <jennnguyen@csumb.edu>
2017-09-22 05:22:48 +0000

    runs for all players(some bugs) + score adding

--- inc/functions.php
+++ inc/functions.php
@@ -1,39 +1,43 @@
 <?php 
- $cardNames = array("clubs","diamonds","hearts","spades"); // folder names (suites)
- $PlayersDeck =array();
- $PlayersSuite=array();
- $deck = range(1,52);
-   shuffle($deck);
-function randomCardFolder($deck)
-{
-    $score=0;
+    $cardNames = array("clubs", "diamonds", "hearts", "spades"); // folder names (suites)
+    $PlayersDeck = array();
+    $PlayersSuite = array();
+    $deck = range(1, 52);
+    shuffle($deck);
+ 
+ 
+    function playerturn($deck) {
+        $score = 0;
     
-   // shuffles #'s
-   //print_r($deck);
-    while($score < 38)
-    {
-        $suite="";
-        $card=array_pop($deck);
-        $tempNum=(($card%13)+1);
-          //echo 'tempNum: '.$tempNum .'<br/>';
-        $suiteValue= (floor($card /13)+1);
-         //echo 'suiteValue: '.$suiteValue .'<br/>';
-        $score = $score +$tempNum;
-         //echo 'score: '.$score .'<br/>';
-        switch($suiteValue)
-        {
-            case 1: $suite="clubs";
-            break;
-            case 2: $suite="diamonds";
-            break;
-            case 3: $suite="hearts";
-            break;
-            case 4: $suite="spades";
-            break;
+        // shuffles #'s
+        //print_r($deck);
+        while($score < 38) {
+            $suite = "";
+            $card = array_pop($deck);
+            $tempNum = (($card % 13) + 1);
+            //echo 'tempNum: '.$tempNum .'<br/>';
+            $suiteValue = (floor($card / 13) + 1);
+            //echo 'suiteValue: '.$suiteValue .'<br/>';
+            $score = $score + $tempNum;
+            //echo 'score: '.$score .'<br/>';
+            switch($suiteValue)
+            {
+                case 1: $suite = "clubs";
+                        break;
+                case 2: $suite = "diamonds";
+                        break;
+                case 3: $suite = "hearts";
+                        break;
+                case 4: $suite = "spades";
+                        break;
+            }
+            echo "<img src='cards/$suite/$tempNum.png' width='70'/>";
+            
         }
-       echo "<img src='cards/$suite/$tempNum.png' width='70'/>";
-       // echo '<br/>';
+        echo '<br/>';
+        return $score;
     }
-    return $score;
-}
+
+
+    
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,70 @@
+<?php
+    include 'inc/functions.php'
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Silver Jack </title>
     </head>
     <body>
-  <?php
-   include 'inc/functions.php';
-  
-  $score=randomCardfolder($deck);
- echo 'score '.$score .'<br/>';
-    ?>
+        <?php
+        
+            //play();
+            // Player 1-4 are dealt cards
+        for ($i = 0; $i < 4; $i++) {
+            $score[$i] = playerturn($deck);
+            echo 'score '.$score[$i].'<br/>';
+            //array_push($score, playerturn($deck));
+        }
+            
+        //$winner
+        $highestScore = 0;
+        $totalScore = 0;
+        $winnerIndex = -1;
+    
+        for ($i = 0; $i < 4; $i++) {
+            if ($score[$i] > 42)
+                continue;
+            if ($score[$i] > $highestScore) {
+                $highestScore = $score[$i];
+                $winnerIndex = $i;
+            }
+        }
+            
+        //IF THERE ARE NO WINNERS !!
+        if ($winnerIndex == -1){
+            echo "<h2>No winners.</h2>"; 
+            return;
+        }
+            
+        for ($i = 0; $i < 4; $i++) {
+            if ($i == $winnerIndex)
+                continue;
+            $totalScore = $totalScore + $score[$i];
+        }
+        
+        $playerWinner = $winnerIndex + 1;
+        switch($playerWinner)
+            {
+                case 1: $winner = "Alex";
+                        break;
+                case 2: $winner = "Jenn";
+                        break;
+                case 3: $winner = "Jose";
+                        break;
+                case 4: $winner = "Mo";
+                        break;
+            }
+        
+        echo "<h2> $winner wins $totalScore points!!</h2>"; 
+        
+        
+        //Print score
+        //echo 'score '.$score.'<br/>';
+        
+            
+        ?>
     </body>
 </html>
