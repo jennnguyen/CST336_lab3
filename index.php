8b6c7b374e7712ebd72af0001487788268e46464
Mohamad Khalifa <mkhalifa@csumb.edu>
2017-09-25 03:52:33 +0000

    final commit

--- index.php
+++ index.php
@@ -5,7 +5,30 @@
 ?>
 
 <?php
-    session_start();
+ session_start();
+ global $temp;
+ $temp=array();
+
+
+    if (!isset($_SESSION['count']))
+    {
+       $_SESSION['count'] = 0;
+        $_SESSION['total']=1;
+       
+        
+      
+    } 
+    else 
+    {
+          $_SESSION['count']++;
+          $_SESSION['total']=$_SESSION['total'];
+         
+         
+      
+         
+    }
+   
+   
     
     include 'inc/functions.php'
     
@@ -24,24 +47,39 @@
     
     <style>
         @import url("css/styles.css");
-        
     </style>
  
     <body>
         <?php
             
-        
+            
             play();
             
-                $start = microtime(true);
-                for ($i = 0; $i < 1000000; $i++) {
-                $j = rand(1,3000) * rand(1,2000);
-            }
+            
 
-                 $elapsedSecs = microtime(true) - $start;
+            
+            
+                $start = microtime(true);
+
+                 for ($i = 0; $i < 1000000; $i++) 
+                 {
+                    $j = rand(1,3000) * rand(1,2000);
+                }
+
+                $elapsedSecs = microtime(true) - $start;
+                $_SESSION['total']+=$eslapsedSecs;
+                $avg_time=$_SESSION['total']/$_SESSION['count'];
                 echo "<div id = 'time'>";
+                
                 echo "Time elapsed: " . $elapsedSecs . " secs";
+                echo "<br />";
+                echo "Average Elapsed time: " . $avg_time . "secs";
+                echo "<br />";
+                echo "Number of games played: " . $_SESSION['count'] . "";
                 echo "</div>";
+                
+                
+            
         ?>
         <br/> <br/>
         <form>
